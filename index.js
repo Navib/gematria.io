77c21115efc091818014c115c3e89fe0f603f405
navib <ivan.baena.dev@gmail.com>
2018-10-29 20:50:07 -0400

    heroku setup v32

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const keys = require('./config/keys');
@@ -13,7 +14,15 @@ app.use(
     keys: [keys.cookieKey]
   })
 );
-app.use(express.static('/public'));
+
+// the __dirname is the current directory from where the script is running
+app.use(express.static('./app/public/'));
+
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('./app/public/', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT);
