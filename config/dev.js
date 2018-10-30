bfa6cd68073d6b00913ecfde18cde1032cb6f7a5
navib <ivan.baena.dev@gmail.com>
2018-10-29 20:53:59 -0400

    fixed mongo error

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
   cookieKey: '2894yt2931gru23gifwaegr0823yrgo',
-  mongoURI: 'mongodb://gematria:gematria1@ds117773.mlab.com:17773/gematria-dev'
+  mongoURI: 'mongodb://admin:password1@ds117773.mlab.com:17773/gematria-dev'
 };
